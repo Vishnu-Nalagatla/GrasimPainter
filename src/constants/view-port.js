c98c1f249fe171f9d3b5666a58674a07f1e3c495
snigdha <snigdhareddy2993@gmail.com>
2023-01-12 11:36:00 +0530

    View port changes

--- src/constants/view-port.js
+++ src/constants/view-port.js
@@ -1,26 +1,5 @@
-// import { createViewPortConfig } from 'react-native-responsive-view-port';
+import { createViewPortConfig } from 'react-native-responsive-view-port';
 
-// const ViewPort = createViewPortConfig(2280, 1080);
+const ViewPort = createViewPortConfig(1080, 2280);
 
-// export default ViewPort;
-
-
-import { Platform, Dimensions } from 'react-native';
-import UTIL from 'mroads-js-utils';
-
-const { width, height } = Dimensions.get('screen');
-
-function createViewPortConfig(baseDeviceWidth = 2280, baseDeviceHeight = 1080) {
-    let screenWidth = width;
-    let screenHeight = height;
-    if (height > width) {
-        screenWidth = height;
-        screenHeight = width;
-    }
-    return {
-        vw: UTIL.round(screenWidth / baseDeviceWidth),
-        vh: UTIL.round(screenHeight / baseDeviceHeight),
-    };
-}
-
-export default createViewPortConfig(2280, Platform.OS === 'android' ? 1080 : 1200);
+export default ViewPort;
