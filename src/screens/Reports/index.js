aba4bbebebb289ca8d6a0de93c47445005e49a6a
snigdha <snigdhareddy2993@gmail.com>
2023-01-19 10:21:55 +0530

    Accordion Component Reports

--- src/screens/Reports/index.js
+++ src/screens/Reports/index.js
@@ -1,9 +1,129 @@
-import {ScrollView} from 'native-base';
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import { ScrollView } from 'native-base';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import SwithcButtons from '../../components/SwithcButtons';
-
+import Accordion from 'react-native-collapsible/Accordion';
 import styles from './styles';
+import upImg from '../../assets/images/splash/upload.png';
+import downImg from '../../assets/images/splash/down.png';
+
+const roomsData = [
+  {
+    name: 'Namita Bedroom',
+    walls: [
+      {
+        name: 'Wall 1',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 2',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 3',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+    ]
+  },
+  {
+    name: 'Living Room',
+    walls: [
+      {
+        name: 'Wall 1',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 2',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 3',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+    ]
+  },
+  {
+    name: 'Kitchen',
+    walls: [
+      {
+        name: 'Wall 1',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 2',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+      {
+        name: 'Wall 3',
+        paintingSystem: [
+          'FS Premium  Emulsion (Acry P)',
+          'Birla White Wall Care Putty',
+        ],
+        addOns: [
+          'Water Proofing',
+          'Design Finish',
+        ]
+      },
+    ]
+  }
+]
+
 
 const Reports = () => {
   const buttons = [
@@ -20,9 +140,12 @@ const Reports = () => {
   ];
 
   const [state, setTabState] = useState(buttons);
+  const [activeRooms, setActiveRooms] = useState([]);
+  const [multipleSelect, setMultipleSelect] = useState(false);
+
   const onClick = event => {
     const buttonsChanged = state.map(button => {
-      const {id} = button;
+      const { id } = button;
       if (event.id === id && !event.status) {
         button.status = true;
       } else {
@@ -33,12 +156,46 @@ const Reports = () => {
     setTabState(buttonsChanged);
   };
 
+  const renderHeader = (wall) => {
+    return (
+      <View style={styles.wallContainer}>
+        <Text style={styles.roomName}>{wall.name}</Text>
+        <Image source={downImg} style={styles.accordionImg} />
+      </View>
+    )
+  }
+
+  const renderContent = (wall) => {
+    return (
+      <View style={[styles.wallContainer, styles.wallDetailsContainer]}>
+        <Text style={styles.roomName}>{wall.name}</Text>
+        <Text style={[styles.roomName, styles.marginStyle]}>Painting System</Text>
+        <View style={styles.separator} />
+        <Text style={[styles.roomName, styles.marginStyle]}>Add Ons</Text>
+        <View style={styles.separator} />
+      </View>
+    )
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.bodyContainer}>
         <SwithcButtons buttons={state} onClick={onClick} />
         <View style={styles.accordionContainer}>
-          <Text> {''}</Text>
+          <Text style={styles.heading}>Product and Colors</Text>
+          <View>
+            <Accordion
+              activeSections={activeRooms}
+              sections={roomsData[0].walls}
+              touchableComponent={TouchableOpacity}
+              renderHeader={renderHeader}
+              renderContent={renderContent}
+              expandMultiple={multipleSelect}
+              onChange={(data) => {
+                setActiveRooms(data);
+              }}
+            />
+          </View>
         </View>
       </View>
     </ScrollView>
--- src/screens/Reports/styles.js
+++ src/screens/Reports/styles.js
@@ -1,20 +1,18 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import ViewPort from '../../constants/view-port';
 import colors from '../../constants/colors';
 
-const {vh, vw} = ViewPort;
+const { vh, vw } = ViewPort;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colors.sliderTrack,
   },
   bodyContainer: {
     flex: 1,
     marginTop: 120 * vh,
-    // backgroundColor: colors.white,
     borderRadius: 20 * vh,
   },
   accordionContainer: {
@@ -22,6 +20,57 @@ const styles = StyleSheet.create({
     marginTop: 100 * vh,
     backgroundColor: colors.white,
     borderRadius: 20 * vh,
+  },
+  heading: {
+    fontFamily: 'Lato',
+    fontSize: 50 * vh,
+    fontWeight: '700',
+    marginTop: 50 * vh,
+    marginBottom: 80 * vh,
+    color: '#000000',
+    opacity: 0.6,
+    letterSpacing: 1 * vw,
+    paddingLeft: 50 * vw,
+  },
+  roomName: {
+    fontSize: 45 * vh,
+    fontFamily: 'Lato',
+    paddingLeft: 50 * vw,
+    fontWeight: '700',
+    color: '#000000',
+    opacity: 0.6,
+    letterSpacing: 1 * vw,
+  },
+  wallContainer: {
+    borderRadius: 25 * vh,
+    borderColor: '#91A9F9',
+    borderWidth: 2 * vh,
+    width: '90%',
+    height: 150 * vh,
+    alignSelf: 'center',
+    marginBottom: 30 * vh,
+    flexDirection: 'row',
+    padding: 40 * vh
+  },
+  wallDetailsContainer: {
+    height: 400 * vh,
+    flexDirection: 'column'
+  },
+  accordionImg: {
+    width: 70 * vw,
+    height: 70 * vh,
+    position: 'absolute',
+    right: 20 * vw,
+    marginTop: 35 * vh,
+  },
+  marginStyle: {
+    marginTop: 40 * vh,
+  },
+  separator: {
+    borderBottomColor: '#E7EBF6',
+    borderBottomWidth: 6 * vh,
+    width: '90%',
+    alignSelf: 'center',
   },
 });
 
