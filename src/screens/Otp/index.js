945134fd1ac3e0e9593832547ba8f2ad2bf54900
snigdha <snigdhareddy2993@gmail.com>
2023-01-23 10:13:30 +0530

    Show resend otp only thrice and maintain storage for logged in user

--- src/screens/Otp/index.js
+++ src/screens/Otp/index.js
@@ -14,6 +14,7 @@ import starIcon from '../../assets/images/splash/star_logo.png';
 import colors from '../../constants/colors';
 import { setLoginData } from '../../store/actions';
 import OTPInputView from '@twotalltotems/react-native-otp-input';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export interface Props {
     navigation: Navigator;
@@ -30,12 +31,17 @@ class Otp extends React.Component<Props, State> {
         this.state = {
             otp: '',
             validationMsg: '',
-            count: 3,
+            count: 5,
             showResend: false,
+            resendCount: 0,
         };
     }
 
     componentDidMount = () => {
+        this.setResendInterval();
+    }
+
+    setResendInterval = () => {
         const interval = setInterval(() => {
             this.setState(prevState => {
                 return {
@@ -56,6 +62,7 @@ class Otp extends React.Component<Props, State> {
         const { route } = this.props;
         const { params } = route;
         const { userName } = params;
+        const { resendCount } = this.state;
         // const userName = '6301451336';
         this.showSpinner();
         API.sendOTP({
@@ -63,9 +70,24 @@ class Otp extends React.Component<Props, State> {
         }).then((response) => {
             const { data } = response;
             if (data && data.statusCode === 1701) {
-                this.closePopup();
+                this.setState({
+                    popup: undefined,
+                    count: 5,
+                    showResend: false,
+                    resendCount: resendCount + 1,
+                }, () => {
+                    this.setResendInterval();
+                })
             } else {
-                this.setState({ validationMsg: data.message, popup: undefined })
+                this.setState({
+                    validationMsg: data.message,
+                    popup: undefined,
+                    count: 5,
+                    showResend: false,
+                    resendCount: resendCount + 1,
+                }, () => {
+                    this.setResendInterval();
+                })
             }
         }).catch((error) => {
             this.setState({ validationMsg: 'Error invoking Send OTP API', popup: undefined });
@@ -107,12 +129,17 @@ class Otp extends React.Component<Props, State> {
         API.verifyOtp(request)
             .then((response) => {
                 const { data } = response;
-                if (data && data.code && data.code === 103) {
+                if (data && data.statusCode && data.statusCode === 103) {
                     this.setState({ validationMsg: data.message })
-                } else if (data.code && data.code === 101) {
-                    const userData = data.response;
+                } else if (data.statusCode && data.statusCode === 101) {
+                    const date = new Date();
+                    const currentDate = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+                    AsyncStorage.setItem('loggedInUser_' + currentDate, userName)
+                        .then(() => {
+                            dispatchSetLoginData({ isLoggedIn: true, loginInfo: { userName: userName } })
+                        });
                     //Here data.response has multiple records, which one to consider
-                    dispatchSetLoginData(userData.records);
+                    // dispatchSetLoginData(userData.records);
                 }
                 this.closePopup();
             })
@@ -140,7 +167,7 @@ class Otp extends React.Component<Props, State> {
     };
 
     render() {
-        const { popup, otp, validationMsg, count, showResend } = this.state;
+        const { popup, otp, validationMsg, count, showResend, resendCount } = this.state;
         const { route } = this.props;
         const { params } = route;
         const { userName, firstName } = params;
@@ -179,9 +206,9 @@ class Otp extends React.Component<Props, State> {
                         <View style={styles.row}>
                             <Text style={styles.userText}>Din't receive the code ? </Text>
                             <Text style={styles.resendOtp} onPress={this.resendOtp}>Resend OTP</Text>
-                        </View>) : (
-                        <Text style={styles.userText}>Didn't receive the code ? Resend in {count} seconds</Text>
-                    )}
+                        </View>) : resendCount < 3 ? (
+                            <Text style={styles.userText}>Didn't receive the code ? Resend in {count} seconds</Text>
+                        ) : null}
                 </View>
                 <Text style={styles.helpText} onPress={this.invokeHelp}>{strings.needHelp}</Text>
             </View >
