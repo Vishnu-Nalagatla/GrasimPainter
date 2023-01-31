6b6f247d7e5c1f815513a44bf6613dc1a71cb84b
snigdha <snigdhareddy2993@gmail.com>
2023-01-31 14:04:53 +0530

    Splash screen restyling

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Container, Content } from 'native-base';
-import { Image, View, Text } from 'react-native';
-import paintIcon from '../../assets/images/splash/paint_logo.png';
-import starIcon from '../../assets/images/splash/star_logo.png';
-import paintCraftIcon from '../../assets/images/splash/paint_logo.png';
-import painterIcon from '../../assets/images/splash/paint_logo.png';
+import { Image, View } from 'react-native';
+import paintLogo from '../../assets/images/splash/paint.png';
 import styles from './styles';
 import { API, SFDC_API } from '../../requests';
 class SplashScreen extends React.Component {
@@ -25,10 +21,7 @@ class SplashScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image source={paintIcon} style={styles.paintIcon} resizeMode="contain" />
-                <Image source={starIcon} style={styles.starIcon} resizeMode="contain" />
-                {/* <Image source={paintCraftIcon} style={styles.paintCraftIcon} resizeMode="contain" />
-                <Image source={painterIcon} style={styles.painterIcon} resizeMode="contain" /> */}
+                <Image source={paintLogo} style={styles.paintIcon} resizeMode="contain" />
             </View>
         );
     }
--- src/screens/SplashScreen/styles.js
+++ src/screens/SplashScreen/styles.js
@@ -12,25 +12,8 @@ const styles = StyleSheet.create({
         backgroundColor: colors.primary,
     },
     paintIcon: {
-        height: 251 * vh,
-        width: 251 * vw
-    },
-    starIcon: {
-        width: 131 * vw,
-        height: 131 * vh,
-        position: 'absolute',
-        left: '53%',
-        top: '50%',
-    },
-    paintCraftIcon: {
-        width: 56.19 * vw,
-        height: 12.46 * vh,
-        marginTop: 60 * vh,
-    },
-    painterIcon: {
-        width: 56.19 * vw,
-        height: 12.46 * vh,
-        marginTop: 30 * vh,
+        height: 146 * vh,
+        width: 146 * vw,
     },
 });
 
