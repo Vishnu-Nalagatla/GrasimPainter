82d01e37bc333699d89d075226d4e3f5538f7f0f
Mahendar <mahendar.kancham@mroads.com>
2023-01-31 13:32:54 +0530

    react-devtools added.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @format
  */
-
+<script src="http://localhost:8097"></script>
 import { AppRegistry } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
