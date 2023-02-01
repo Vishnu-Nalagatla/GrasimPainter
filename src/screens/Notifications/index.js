62122e5df1283621ff9a4163588f8d24c785d641
Mahendar <mahendar.kancham@mroads.com>
2023-02-01 18:23:17 +0530

    notifications screen added.

--- src/screens/Notifications/index.js
+++ src/screens/Notifications/index.js
@@ -1,13 +1,92 @@
-import {Text} from 'native-base';
+import {FlatList, Image, ScrollView, Text} from 'native-base';
 import React from 'react';
 import {View} from 'react-native';
+import complaintsImg from '../../assets/images/complaints/image.png';
 
 import styles from './styles';
 
 const Notifications = () => {
+  const notifications = [
+    {
+      type: 'complaint',
+      message:
+        'Ravi Kumar from The Mehtas has requested to change project timeline',
+    },
+    {
+      type: 'complaint',
+      message:
+        'The Mehtas are now have a updated timeline. Please check what are next items.',
+    },
+    {
+      type: 'newProject',
+      message:
+        'New Project “The Mehtas” has been assigned to you. Tap to view the details',
+    },
+    {
+      type: 'leaveRequest',
+      message: 'Leave request from Painter Ravi Verma for 3 days',
+    },
+    {
+      type: 'leaveRequest',
+      message: 'Leave request from Painter Ravi Verma for 3 days',
+    },
+    {
+      type: 'siteVisit',
+      message:
+        'Team Lead Mukesh soni has asked to retake few images for The Mehtas - Living Room',
+    },
+    {
+      type: 'siteVisit',
+      message:
+        'Team Lead Mukesh soni has asked to retake few images for The Mehtas - Living Room',
+    },
+    {
+      type: 'leaveRequest',
+      message: 'Leave request from Painter Ravi Verma for 3 days',
+    },
+    {
+      type: 'leaveRequest',
+      message: 'Leave request from Painter Ravi Verma for 3 days',
+    },
+    {
+      type: 'siteVisit',
+      message:
+        'Team Lead Mukesh soni has asked to retake few images for The Mehtas - Living Room',
+    },
+    {
+      type: 'siteVisit',
+      message:
+        'Team Lead Mukesh soni has asked to retake few images for The Mehtas - Living Room',
+    },
+  ];
+  const Notification = ({notification}) => {
+    const {type, message} = notification;
+    return (
+      <View style={styles.notificationCard}>
+        <View style={styles.notification}>
+          <Image
+            source={complaintsImg}
+            style={styles.notifiaftionImg}
+            resizeMode="contain"
+          />
+          <Text style={styles.notifiaftionLable}>{message}</Text>
+        </View>
+        <Text style={styles.time}>{'9 hours ago'}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{'Notifications'}</Text>
+      <ScrollView contentContainerStyle={styles.bodyContainer}>
+        <FlatList
+          data={notifications}
+          keyExtractor={(item, index) => item.index}
+          renderItem={({item, index}) => (
+            <Notification notification={item} index={index} />
+          )}
+        />
+      </ScrollView>
     </View>
   );
 };
--- src/screens/Notifications/styles.js
+++ src/screens/Notifications/styles.js
@@ -7,9 +7,47 @@ const {vh, vw} = ViewPort;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: 360 * vw,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colors.sliderTrack,
+  },
+
+  bodyContainer: {
+    width: 356 * vw,
+    marginLeft: 4 * vw,
+    backgroundColor: colors.cardBackground,
+  },
+  notificationCard: {
+    height: 88 * vh,
+    width: '100%',
+    borderWidth: 1 * vh,
+    borderColor: '#DCE3F8',
+  },
+  notification: {
+    flexDirection: 'row',
+    padding: 8 * vh,
+  },
+  notifiaftionLable: {
+    width: '90%',
+    fontFamily: 'Karla',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 14 * vh,
+    paddingRight: 12 * vh,
+  },
+  time: {
+    fontFamily: 'Karla',
+    fontStyle: 'normal',
+    fontWeight: '400',
+    fontSize: 10 * vh,
+    paddingLeft: 48 * vh,
+  },
+  notifiaftionImg: {
+    height: 24 * vh,
+    width: 24 * vw,
+    marginRight: 12 * vh,
+    marginTop: 10 * vh,
   },
 });
 
