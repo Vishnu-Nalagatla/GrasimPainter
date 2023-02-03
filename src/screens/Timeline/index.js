3c43e5b404e46ae230fd2842b845cf0cc74780e9
snigdha <snigdhareddy2993@gmail.com>
2023-02-03 13:40:36 +0530

    Timeline Calender styling

--- src/screens/Timeline/index.js
+++ src/screens/Timeline/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import ellipse from '../../assets/images/ellipse/image.png';
 import hamburger from '../../assets/images/timeline/reorder.png';
@@ -13,255 +13,298 @@ import POPUP_CONSTANTS from '../../enums/popup';
 import data from './data.json';
 
 class Timeline extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            // timeLineData: initialData.roomSequence,
-            timeLineData: [],
-            projectStartDate: data.ProjectStartDate,
-            projectEndDate: data.ProjectEndDate,
-            popup: undefined,
-        };
-    }
-
-    componentDidMount = () => {
-        this.getTimeLineSequence();
-    }
-
-    compare(room1, room2) {
-        if (room1.RoomSequence < room2.RoomSequence) {
-            return 1;
-        }
-        if (room1.RoomSequence > room2.RoomSequence) {
-            return -1;
-        }
-        return 0;
-    }
-
-    getTimeLineSequence = () => {
-        const roomsList = data.RoomList;
-        let sequencedArray = roomsList.sort(this.compare);
-        console.log('sequencedArray', sequencedArray);
-        const roomOrderedList = this.getCalculatedTimelineSequence(sequencedArray);
-        this.setState({ timeLineData: roomOrderedList.reverse() });
-    }
-
-    getCalculatedTimelineSequence = (roomsList) => {
-        const calculatedTimelineSequence = [];
-        const { projectStartDate } = this.state;
-
-        for (var roomIdx = roomsList.length - 1; roomIdx >= 0; roomIdx--) {
-            let DaysForPrevRooms = 0;
-            let roomStartDate = '';
-            if (roomIdx === roomsList.length - 1) {
-                roomStartDate = this.getFormattedDate();
-                DaysForPrevRooms = 0;
-            } else {
-                DaysForPrevRooms = calculatedTimelineSequence[calculatedTimelineSequence.length - 1].DaysForPrevRooms + calculatedTimelineSequence[calculatedTimelineSequence.length - 1].DaysRequiredForRoom;
-                roomStartDate = this.getFormattedDate(DaysForPrevRooms);
-            }
-            calculatedTimelineSequence.push({
-                ...roomsList[roomIdx],
-                roomStartDate,
-                DaysForPrevRooms,
-            })
-        }
-        console.log('calculatedTimelineSequence', calculatedTimelineSequence);
-        return calculatedTimelineSequence;
-    }
-
-    getFormattedDate = (roomDuration) => {
-        const { projectStartDate } = this.state;
-        var date = new Date(projectStartDate);
-        let nextRoomStartDate;
-        if (roomDuration) {
-            nextRoomStartDate = new Date(date.setDate(date.getDate() + Math.floor(roomDuration)));
-        } else {
-            nextRoomStartDate = date;
-        }
-        var formattedDate = nextRoomStartDate.getDate() + ' ' + nextRoomStartDate.toString().substr(4, 3) + ' ' + nextRoomStartDate.getFullYear();
-        return formattedDate;
-    }
-
-    onStartDateChange = (date) => {
-        const { timeLineData } = this.state;
-        const selectedDate = new Date(date);
-        var formatDate = selectedDate.getDate() + ' ' + selectedDate.toString().substr(4, 3) + ' ' + selectedDate.getFullYear();
-        timeLineData[timeLineData.length - 1].roomStartDate = formatDate;
-        this.setState({
-            projectStartDate: selectedDate.getFullYear() + '-' + (selectedDate.getMonth() + 1) + '-' + selectedDate.getDate(),
-            timeLineData: timeLineData,
-            popup: undefined,
-        })
-    }
-
-    onEndDateChange = date => {
-        const { timeLineData } = this.state;
-        const selectedDate = new Date(date);
-        var formatDate = selectedDate.getDate() + ' ' + selectedDate.toString().substr(4, 3) + ' ' + selectedDate.getFullYear();
-        timeLineData[0].roomStartDate = formatDate;
-        this.setState({
-            projectEndDate: selectedDate.getFullYear() + '-' + selectedDate.getMonth() + '-' + selectedDate.getDate(),
-            timeLineData: timeLineData,
-            popup: undefined,
-        })
-    }
-
-    showCalendar = (index) => {
-        const { timeLineData } = this.state;
-        if (index === 0)
-            this.setState({ type: POPUP_CONSTANTS.SHOW_END_DATE_CALENDAR });
-        else if (index === timeLineData.length - 1)
-            this.setState({ type: POPUP_CONSTANTS.SHOW_START_DATE_CALENDAR });
-    }
-
-    renderItem = (data) => {
-        const { item, drag } = data;
-        const { totalTime = 0 } = item;
-        const { timeLineData } = this.state;
-        const index = data.getIndex();
-        return (
-            <TouchableOpacity style={styles.item}
-                onPress={() => this.showCalendar(index)}
-                onLongPress={drag}
-            >
-                <View style={styles.projectLine}>
-                    <Image
-                        source={ellipse}
-                        style={styles.ellipse}
-                        resizeMode="contain"
-                    />
-                    <View style={styles.straightLine} />
-                </View>
-                <View style={styles.textContainer}>
-                    <Text style={[styles.heading, styles.roomHeading, styles.noMargin]}>{item.Name}</Text>
-                    <View style={styles.dateContainer}>
-                        {/* <View style={styles.dateContainer} onPress={() => showCalendar(index)}> */}
-                        <Image
-                            source={flagImg}
-                            style={styles.flagImg}
-                            resizeMode="contain"
-                        />
-                        <Text onPress={() => this.showCalendar(index)} style={index === 0 || index === timeLineData.length - 1 ? [styles.startDate, styles.highlight] : styles.startDate}>{item.roomStartDate}</Text>
-                    </View>
-                </View>
-                <View style={styles.hamburgerContainer}>
-                    <Image
-                        source={hamburger}
-                        style={styles.ellipse}
-                        resizeMode="contain"
-                    />
-                </View>
-            </TouchableOpacity >
-        )
-    }
-
-    updateProjectPlan = () => {
-
-    }
-
-    recalculateProjectPlan = () => {
-
-    }
-
-    setTimelineSequence = data => {
-        console.log('setTimelineSequence data', data);
-        const updatedSequence = this.getCalculatedTimelineSequence(data);
-        console.log('setTimelineSequence updatedSequence data', updatedSequence);
-        this.setState({ timeLineData: updatedSequence.reverse() });
-    }
-
-    getPopupContent = () => {
-        const { popup, projectStartDate } = this.state;
-        if (!popup) {
-            return null;
-        }
-        switch (popup.type) {
-            case POPUP_CONSTANTS.SHOW_START_DATE_CALENDAR:
-                return <CalendarPicker onDateChange={(date) => this.onStartDateChange(date)}
-                    previousComponent={this.getPreviousComponent()}
-                    nextComponent={this.getNextComponent()}
-                    startFromMonday={true}
-                    showDayStragglers={true}
-                    selectedDayColor='#2C4DAE'
-                    textStyle={styles.textStyle}
-                    minDate={new Date()}
-                />;
-            case POPUP_CONSTANTS.SHOW_END_DATE_CALENDAR:
-                return <CalendarPicker onDateChange={(date) => this.onEndDateChange(date)}
-                    previousComponent={this.getPreviousComponent()}
-                    nextComponent={this.getNextComponent()}
-                    startFromMonday={true}
-                    showDayStragglers={true}
-                    selectedDayColor='#2C4DAE'
-                    textStyle={styles.textStyle}
-                    minDate={new Date(projectStartDate)}// replace with project start date
-                />;
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      // timeLineData: initialData.roomSequence,
+      timeLineData: [],
+      projectStartDate: data.ProjectStartDate,
+      projectEndDate: data.ProjectEndDate,
+      popup: undefined,
     };
+  }
 
-    getPreviousComponent = () => {
-        return (
-            <View style={styles.buttonStyle}>
-                <Image
-                    source={ellipse}
-                    style={styles.imgStyle}
-                    resizeMode="contain"
-                />
-            </View>
-        )
+  componentDidMount = () => {
+    this.getTimeLineSequence();
+  };
+
+  compare(room1, room2) {
+    if (room1.RoomSequence < room2.RoomSequence) {
+      return 1;
     }
-
-    getNextComponent = () => {
-        return (
-            <View style={styles.buttonStyle}>
-                <Image
-                    source={ellipse}
-                    style={styles.imgStyle}
-                    resizeMode="contain"
-                />
-            </View>
-        )
+    if (room1.RoomSequence > room2.RoomSequence) {
+      return -1;
     }
+    return 0;
+  }
 
-    render() {
-        const { popup, timeLineData } = this.state;
+  getTimeLineSequence = () => {
+    const roomsList = data.RoomList;
+    let sequencedArray = roomsList.sort(this.compare);
+    console.log('sequencedArray', sequencedArray);
+    const roomOrderedList = this.getCalculatedTimelineSequence(sequencedArray);
+    this.setState({timeLineData: roomOrderedList.reverse()});
+  };
+
+  getCalculatedTimelineSequence = roomsList => {
+    const calculatedTimelineSequence = [];
+    const {projectStartDate} = this.state;
+
+    for (var roomIdx = roomsList.length - 1; roomIdx >= 0; roomIdx--) {
+      let DaysForPrevRooms = 0;
+      let roomStartDate = '';
+      if (roomIdx === roomsList.length - 1) {
+        roomStartDate = this.getFormattedDate();
+        DaysForPrevRooms = 0;
+      } else {
+        DaysForPrevRooms =
+          calculatedTimelineSequence[calculatedTimelineSequence.length - 1]
+            .DaysForPrevRooms +
+          calculatedTimelineSequence[calculatedTimelineSequence.length - 1]
+            .DaysRequiredForRoom;
+        roomStartDate = this.getFormattedDate(DaysForPrevRooms);
+      }
+      calculatedTimelineSequence.push({
+        ...roomsList[roomIdx],
+        roomStartDate,
+        DaysForPrevRooms,
+      });
+    }
+    console.log('calculatedTimelineSequence', calculatedTimelineSequence);
+    return calculatedTimelineSequence;
+  };
+
+  getFormattedDate = roomDuration => {
+    const {projectStartDate} = this.state;
+    var date = new Date(projectStartDate);
+    let nextRoomStartDate;
+    if (roomDuration) {
+      nextRoomStartDate = new Date(
+        date.setDate(date.getDate() + Math.floor(roomDuration)),
+      );
+    } else {
+      nextRoomStartDate = date;
+    }
+    var formattedDate =
+      nextRoomStartDate.getDate() +
+      ' ' +
+      nextRoomStartDate.toString().substr(4, 3) +
+      ' ' +
+      nextRoomStartDate.getFullYear();
+    return formattedDate;
+  };
+
+  onStartDateChange = date => {
+    const {timeLineData} = this.state;
+    const selectedDate = new Date(date);
+    var formatDate =
+      selectedDate.getDate() +
+      ' ' +
+      selectedDate.toString().substr(4, 3) +
+      ' ' +
+      selectedDate.getFullYear();
+    timeLineData[timeLineData.length - 1].roomStartDate = formatDate;
+    this.setState({
+      projectStartDate:
+        selectedDate.getFullYear() +
+        '-' +
+        (selectedDate.getMonth() + 1) +
+        '-' +
+        selectedDate.getDate(),
+      timeLineData: timeLineData,
+      popup: undefined,
+    });
+  };
+
+  onEndDateChange = date => {
+    const {timeLineData} = this.state;
+    const selectedDate = new Date(date);
+    var formatDate =
+      selectedDate.getDate() +
+      ' ' +
+      selectedDate.toString().substr(4, 3) +
+      ' ' +
+      selectedDate.getFullYear();
+    timeLineData[0].roomStartDate = formatDate;
+    this.setState({
+      projectEndDate:
+        selectedDate.getFullYear() +
+        '-' +
+        selectedDate.getMonth() +
+        '-' +
+        selectedDate.getDate(),
+      timeLineData: timeLineData,
+      popup: undefined,
+    });
+  };
+
+  showCalendar = index => {
+    const {timeLineData} = this.state;
+    if (index === 0)
+      this.setState({type: POPUP_CONSTANTS.SHOW_END_DATE_CALENDAR});
+    else if (index === timeLineData.length - 1)
+      this.setState({type: POPUP_CONSTANTS.SHOW_START_DATE_CALENDAR});
+  };
+
+  renderItem = data => {
+    const {item, drag} = data;
+    const {totalTime = 0} = item;
+    const {timeLineData} = this.state;
+    const index = data.getIndex();
+    return (
+      <TouchableOpacity
+        style={styles.item}
+        onPress={() => this.showCalendar(index)}
+        onLongPress={drag}>
+        <View style={styles.projectLine}>
+          <Image source={ellipse} style={styles.ellipse} resizeMode="contain" />
+          <View style={styles.straightLine} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={[styles.heading, styles.roomHeading, styles.noMargin]}>
+            {item.Name}
+          </Text>
+          <View style={styles.dateContainer}>
+            {/* <View style={styles.dateContainer} onPress={() => showCalendar(index)}> */}
+            <Image
+              source={flagImg}
+              style={styles.flagImg}
+              resizeMode="contain"
+            />
+            <Text
+              onPress={() => this.showCalendar(index)}
+              style={
+                index === 0 || index === timeLineData.length - 1
+                  ? [styles.startDate, styles.highlight]
+                  : styles.startDate
+              }>
+              {item.roomStartDate}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.hamburgerContainer}>
+          <Image
+            source={hamburger}
+            style={styles.ellipse}
+            resizeMode="contain"
+          />
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  updateProjectPlan = () => {};
+
+  recalculateProjectPlan = () => {};
+
+  setTimelineSequence = data => {
+    console.log('setTimelineSequence data', data);
+    const updatedSequence = this.getCalculatedTimelineSequence(data);
+    console.log('setTimelineSequence updatedSequence data', updatedSequence);
+    this.setState({timeLineData: updatedSequence.reverse()});
+  };
+
+  getPopupContent = () => {
+    const {popup, projectStartDate} = this.state;
+    if (!popup) {
+      return null;
+    }
+    switch (popup.type) {
+      case POPUP_CONSTANTS.SHOW_START_DATE_CALENDAR:
         return (
-            <View style={styles.container}>
-                <Popup visible={!!popup}>
-                    {this.getPopupContent()}
-                </Popup>
-                <View style={styles.innerContainer}>
-                    <Text style={styles.heading}>{strings.reorderText}</Text>
-                    <View style={styles.listStyle}>
-                        <DraggableFlatList
-                            data={timeLineData}
-                            renderItem={this.renderItem}
-                            keyExtractor={(item, index) => index.toString()}
-                            onDragEnd={({ data }) => {
-                                this.setTimelineSequence(data);
-                            }}
-                        // containerStyle={styles.listStyle}
-                        />
-                    </View>
-                    <CustomButton
-                        title={strings.recalculateProjectPlan}
-                        textStyle={styles.recalculateButtonText}
-                        style={styles.recalculateButton}
-                        onPress={this.recalculateProjectPlan}
-                    />
-                </View>
-                <CustomButton
-                    title={strings.updateProjectPlan}
-                    textStyle={styles.buttonText}
-                    style={styles.button}
-                    onPress={this.updateProjectPlan}
-                />
-            </View>
+          <CalendarPicker
+            onDateChange={date => this.onStartDateChange(date)}
+            previousComponent={this.getPreviousComponent()}
+            nextComponent={this.getNextComponent()}
+            startFromMonday={true}
+            showDayStragglers={true}
+            selectedDayColor="#2C4DAE"
+            textStyle={styles.textStyle}
+            weekdays={['M', 'T', 'W', 'T', 'F', 'S', 'S']}
+            minDate={new Date()}
+          />
+        );
+      case POPUP_CONSTANTS.SHOW_END_DATE_CALENDAR:
+        return (
+          <CalendarPicker
+            onDateChange={date => this.onEndDateChange(date)}
+            previousComponent={this.getPreviousComponent()}
+            nextComponent={this.getNextComponent()}
+            startFromMonday={true}
+            showDayStragglers={true}
+            selectedDayColor="#2C4DAE"
+            textStyle={styles.textStyle}
+            weekdays={['M', 'T', 'W', 'T', 'F', 'S', 'S']}
+            minDate={new Date(projectStartDate)} // replace with project start date
+          />
         );
     }
+  };
 
-};
+  getPreviousComponent = () => {
+    return (
+      <View style={styles.buttonStyle}>
+        <Image source={ellipse} style={styles.imgStyle} resizeMode="contain" />
+      </View>
+    );
+  };
+
+  getNextComponent = () => {
+    return (
+      <View style={styles.buttonStyle}>
+        <Image source={ellipse} style={styles.imgStyle} resizeMode="contain" />
+      </View>
+    );
+  };
+
+  getPopupStyle = popup => {
+    let popupStyle = {};
+    if (
+      popup &&
+      (popup.type === POPUP_CONSTANTS.SHOW_START_DATE_CALENDAR ||
+        popup.type === POPUP_CONSTANTS.SHOW_END_DATE_CALENDAR)
+    ) {
+      popupStyle = styles.calendarStyle;
+    }
+    return popupStyle;
+  };
+
+  render() {
+    const {popup, timeLineData} = this.state;
+    return (
+      <View style={styles.container}>
+        <Popup visible={!!popup} popupStyle={this.getPopupStyle(popup)}>
+          {this.getPopupContent()}
+        </Popup>
+        <View style={styles.innerContainer}>
+          <Text style={styles.heading}>{strings.reorderText}</Text>
+          <View style={styles.listStyle}>
+            <DraggableFlatList
+              data={timeLineData}
+              renderItem={this.renderItem}
+              keyExtractor={(item, index) => index.toString()}
+              onDragEnd={({data}) => {
+                this.setTimelineSequence(data);
+              }}
+              // containerStyle={styles.listStyle}
+            />
+          </View>
+          <CustomButton
+            title={strings.recalculateProjectPlan}
+            textStyle={styles.recalculateButtonText}
+            style={styles.recalculateButton}
+            onPress={this.recalculateProjectPlan}
+          />
+        </View>
+        <CustomButton
+          title={strings.updateProjectPlan}
+          textStyle={styles.buttonText}
+          style={styles.button}
+          onPress={this.updateProjectPlan}
+        />
+      </View>
+    );
+  }
+}
 
 export default Timeline;
--- src/screens/Timeline/styles.js
+++ src/screens/Timeline/styles.js
@@ -1,138 +1,142 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 import ViewPort from '../../constants/view-port';
 import colors from '../../constants/colors';
 
-const { vh, vw } = ViewPort;
+const {vh, vw} = ViewPort;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#e7ebf6',
-        alignItems: 'center',
-    },
-    innerContainer: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        width: 328 * vw,
-        borderRadius: 8 * vw,
-        marginBottom: 10 * vh,
-        marginTop: 20 * vh,
-        paddingLeft: 16 * vw,
-    },
-    heading: {
-        fontFamily: 'Lato-Bold',
-        fontSize: 16 * vh,
-        lineHeight: 24 * vh,
-        marginTop: 16 * vh,
-        marginBottom: 24 * vh,
-        color: '#000000',
-        opacity: 0.6,
-        letterSpacing: 0.25 * vw,
-    },
-    roomHeading: {
-        marginLeft: 16 * vw,
-    },
-    noMargin: {
-        marginTop: 0,
-        marginBottom: 0,
-    },
-    ellipse: {
-        height: 24 * vh,
-        width: 24 * vh,
-    },
-    straightLine: {
-        backgroundColor: '#D0D7E8',
-        height: 60 * vh,
-        width: 3 * vw,
-        marginLeft: 10 * vw,
-    },
-    item: {
-        flex: 1,
-        flexDirection: 'row',
-        width: 295 * vw,
-    },
-    hamburgerContainer: {
-        justifyContent: 'center'
-    },
-    textContainer: {
-        flex: 1,
-    },
-    startDate: {
-        fontFamily: 'Karla-Bold',
-        fontSize: 14 * vh,
-        color: '#000000',
-        opacity: 0.6,
-        letterSpacing: 0.15 * vw,
-    },
-    dateContainer: {
-        flexDirection: 'row',
-        marginTop: 14 * vh,
-        marginLeft: 16 * vw,
-        alignItems: 'center',
-    },
-    flagImg: {
-        height: 16 * vh,
-        width: 16 * vh,
-        marginRight: 10 * vw,
-    },
-    buttonText: {
-        fontSize: 16 * vh,
-        fontFamily: 'Karla-Bold',
-        textAlign: 'center',
-    },
-    button: {
-        height: 42 * vh,
-        width: 328 * vw,
-        justifyContent: 'center',
-        alignSelf: 'center',
-        borderRadius: 8,
-        marginBottom: 20 * vh,
-    },
-    recalculateButton: {
-        height: 42 * vh,
-        width: 290 * vw,
-        alignSelf: 'center',
-        borderRadius: 8,
-        position: 'absolute',
-        bottom: 10 * vh,
-        backgroundColor: '#FFFFFF',
-        borderWidth: 1,
-        borderColor: '#2C4DAE',
-    },
-    recalculateButtonText: {
-        color: '#2C4DAE',
-        fontSize: 14 * vh,
-        lineHeight: 18 * vh,
-        fontFamily: 'Karla-Bold',
-        textAlign: 'center',
-    },
-    listStyle: {
-        width: 328 * vw,
-        height: 380 * vh,
-    },
-    highlight: {
-        color: '#2C4DAE',
-        fontSize: 16 * vh,
-        textDecorationLine: 'underline',
-    },
-    buttonStyle: {
-        backgroundColor: '#e7ebf6',
-        width: 24 * vw,
-        height: 24 * vh,
-        borderRadius: 10 * vh,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    imgStyle: {
-        width: 24 * vw,
-        height: 24 * vh,
-    },
-    textStyle: {
-        fontFamily: 'Karla-Regular',
-        fontSize: 16 * vh,
-        alignItems: 'center',
-        textAlign: 'center',
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#e7ebf6',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    width: 328 * vw,
+    borderRadius: 8 * vw,
+    marginBottom: 10 * vh,
+    marginTop: 20 * vh,
+    paddingLeft: 16 * vw,
+  },
+  heading: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 16 * vh,
+    lineHeight: 24 * vh,
+    marginTop: 16 * vh,
+    marginBottom: 24 * vh,
+    color: '#000000',
+    opacity: 0.6,
+    letterSpacing: 0.25 * vw,
+  },
+  roomHeading: {
+    marginLeft: 16 * vw,
+  },
+  noMargin: {
+    marginTop: 0,
+    marginBottom: 0,
+  },
+  ellipse: {
+    height: 24 * vh,
+    width: 24 * vh,
+  },
+  straightLine: {
+    backgroundColor: '#D0D7E8',
+    height: 60 * vh,
+    width: 3 * vw,
+    marginLeft: 10 * vw,
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    width: 295 * vw,
+  },
+  hamburgerContainer: {
+    justifyContent: 'center',
+  },
+  textContainer: {
+    flex: 1,
+  },
+  startDate: {
+    fontFamily: 'Karla-Bold',
+    fontSize: 14 * vh,
+    color: '#000000',
+    opacity: 0.6,
+    letterSpacing: 0.15 * vw,
+  },
+  dateContainer: {
+    flexDirection: 'row',
+    marginTop: 14 * vh,
+    marginLeft: 16 * vw,
+    alignItems: 'center',
+  },
+  flagImg: {
+    height: 16 * vh,
+    width: 16 * vh,
+    marginRight: 10 * vw,
+  },
+  buttonText: {
+    fontSize: 16 * vh,
+    fontFamily: 'Karla-Bold',
+    textAlign: 'center',
+  },
+  button: {
+    height: 42 * vh,
+    width: 328 * vw,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    borderRadius: 8,
+    marginBottom: 20 * vh,
+  },
+  recalculateButton: {
+    height: 42 * vh,
+    width: 290 * vw,
+    alignSelf: 'center',
+    borderRadius: 8,
+    position: 'absolute',
+    bottom: 10 * vh,
+    backgroundColor: '#FFFFFF',
+    borderWidth: 1,
+    borderColor: '#2C4DAE',
+  },
+  recalculateButtonText: {
+    color: '#2C4DAE',
+    fontSize: 14 * vh,
+    lineHeight: 18 * vh,
+    fontFamily: 'Karla-Bold',
+    textAlign: 'center',
+  },
+  listStyle: {
+    width: 328 * vw,
+    height: 380 * vh,
+  },
+  highlight: {
+    color: '#2C4DAE',
+    fontSize: 16 * vh,
+    textDecorationLine: 'underline',
+  },
+  buttonStyle: {
+    backgroundColor: '#e7ebf6',
+    width: 24 * vw,
+    height: 24 * vh,
+    borderRadius: 10 * vh,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imgStyle: {
+    width: 24 * vw,
+    height: 24 * vh,
+  },
+  textStyle: {
+    fontSize: 14 * vh,
+    lineHeight: 24 * vh,
+    color: '#000000',
+    fontFamily: 'Karla-Bold',
+  },
+  calendarStyle: {
+    height: 324 * vh,
+    padding: 16 * vh,
+  },
 });
 
 export default styles;
