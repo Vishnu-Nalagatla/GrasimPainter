fb186ef3517e20cbd274f712e89ac87ca9e415fc
snigdha <snigdhareddy2993@gmail.com>
2023-02-09 11:13:08 +0530

    Profile projects

---
+++ src/screens/ProfileProjects/index.js
@@ -0,0 +1,159 @@
+import {View, FlatList} from 'native-base';
+import React from 'react';
+import {Image, Text, TouchableOpacity} from 'react-native';
+import styles from './styles';
+import data from './data.json';
+import {Dropdown} from 'react-native-element-dropdown';
+import checkImg from '../../assets/images/myProjects/check.png';
+import flagImg from '../../assets/images/timeline/flag.png';
+import projectImg from '../../assets/images/myProjects/projectImg.png';
+import RouteConfig from '../../constants/route-config';
+
+const statuses = [
+  {label: 'All', value: 'All'},
+  {label: 'Completed', value: 'Completed'},
+  {label: 'New', value: 'New'},
+  {label: 'On Hold', value: 'On Hold'},
+  {label: 'Conflict', value: 'Conflict'},
+  {label: 'QC Visit', value: 'QC Visit'},
+  {label: 'Paid', value: 'Paid'},
+  {label: 'In Progress', value: 'In Progress'},
+];
+
+class ProfileProjects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: '',
+      projectsList: data,
+    };
+  }
+
+  setStatus = status => {
+    const {projectsList} = this.state;
+    const filteredList = projectsList.filter(each => each.Status === status);
+    this.setState({status: status, projectsList: filteredList});
+  };
+
+  renderItem = item => {
+    console.log('render item', item);
+    const {status} = this.state;
+    if (status === item.value) {
+      return (
+        <View style={styles.item}>
+          <Text style={styles.textStyle}>{item.value}</Text>
+          <Image source={checkImg} style={styles.checkImg} />
+        </View>
+      );
+    }
+    return (
+      <View style={styles.item}>
+        <Text style={styles.textStyle}>{item.value}</Text>
+      </View>
+    );
+  };
+
+  navigateToDetails = project => {
+    const {navigation} = this.props;
+    navigation.navigate(RouteConfig.ProfileProjectDetails, {
+      project: project,
+    });
+  };
+
+  render() {
+    const {projectsList} = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.totalProjects}>Total Projects {data.length}</Text>
+          <Dropdown
+            style={styles.dropdown}
+            selectedTextStyle={styles.placeholder}
+            data={statuses}
+            labelField="label"
+            valueField="value"
+            onChange={item => {
+              this.setStatus(item.value);
+            }}
+            renderRightIcon={() => {
+              return <></>;
+            }}
+            value={() => {
+              return <Text>Filter</Text>;
+            }}
+            placeholder="Filter"
+            placeholderStyle={styles.placeholder}
+            renderItem={this.renderItem}
+          />
+        </View>
+
+        <FlatList
+          data={projectsList}
+          keyExtractor={(item, index) => item.Id}
+          renderItem={({item, index}) => (
+            <ProjectCard project={item} onPress={this.navigateToDetails} />
+          )}
+        />
+      </View>
+    );
+  }
+}
+
+const ProjectCard = props => {
+  const {project, onPress} = props;
+
+  const getStatusColor = () => {
+    let statusColor = '';
+    const status = project.Status;
+    if (status === 'Completed') {
+      statusColor = '#70B39D';
+    } else if (
+      status === 'New' ||
+      status === 'On Hold' ||
+      status === 'QC Visit' ||
+      status === '1st Payment'
+    ) {
+      statusColor = '#F3D467';
+    } else if (status === 'Conflict') {
+      statusColor = '#F36C7C';
+    } else if (status === 'In Progress') {
+      statusColor = '#2C4DAE';
+    }
+    return statusColor;
+  };
+
+  var date = new Date(project.ProjectEndDate);
+  var formattedDate =
+    date.getDate() +
+    ' ' +
+    date.toString().substr(4, 3) +
+    ' ' +
+    date.getFullYear();
+  const statusColor = getStatusColor();
+  return (
+    <TouchableOpacity
+      style={styles.cardContainer}
+      onPress={() => onPress(project)}>
+      <Image
+        source={projectImg}
+        style={styles.projectImg}
+        resizeMode="contain"
+      />
+      <View>
+        <View style={styles.header}>
+          <Text style={styles.textStyle}>{project.Name}</Text>
+          <View
+            style={[styles.statusContainer, {backgroundColor: statusColor}]}>
+            <Text style={styles.status}>{project.Status}</Text>
+          </View>
+        </View>
+        <View style={styles.row}>
+          <Image source={flagImg} style={styles.flagImg} resizeMode="contain" />
+          <Text style={styles.endText}>Ended on {formattedDate}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ProfileProjects;
---
+++ src/screens/ProfileProjects/styles.js
@@ -0,0 +1,121 @@
+import {StyleSheet} from 'react-native';
+import ViewPort from '../../constants/view-port';
+
+const {vh, vw} = ViewPort;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+  dropdown: {
+    width: 126 * vw,
+    height: 36 * vh,
+    backgroundColor: '#2C4DAE',
+    borderRadius: 8 * vh,
+    position: 'absolute',
+    right: 0,
+    marginTop: 20 * vh,
+  },
+  totalProjects: {
+    marginLeft: 16 * vw,
+    marginTop: 24 * vh,
+    fontFamily: 'Lato-Bold',
+    fontSize: 14 * vh,
+    lineHeight: 22 * vh,
+    letterSpacing: 0.25 * vw,
+    color: '#000000',
+  },
+  item: {
+    width: 138 * vw,
+    height: 46 * vh,
+    backgroundColor: '#F5F8FF',
+    flexDirection: 'row',
+  },
+  textStyle: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 14 * vh,
+    lineHeight: 22 * vh,
+    letterSpacing: 0.25 * vw,
+    color: '#000000',
+    opacity: 0.6,
+    marginBottom: 12 * vh,
+    marginLeft: 12 * vw,
+  },
+  checkImg: {
+    width: 24 * vw,
+    height: 24 * vh,
+    position: 'absolute',
+    right: 24 * vw,
+  },
+  cardContainer: {
+    width: 328 * vw,
+    height: 66 * vh,
+    borderRadius: 10 * vh,
+    backgroundColor: '#FFFFFF',
+    marginBottom: 12 * vh,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textContainer: {
+    marginBottom: 24 * vh,
+    flexDirection: 'row',
+    width: 328 * vw,
+    alignSelf: 'center',
+  },
+  projectImg: {
+    width: 96 * vw,
+    height: 50 * vh,
+    borderRadius: 8 * vh,
+    marginLeft: 8 * vw,
+    marginRight: 12 * vw,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  header: {
+    flexDirection: 'row',
+    width: 224 * vw,
+  },
+  statusContainer: {
+    width: 78 * vw,
+    height: 20 * vh,
+    backgroundColor: '#70B39D',
+    borderRadius: 15 * vh,
+    position: 'absolute',
+    right: 20 * vw,
+    paddingVertical: 2 * vh,
+  },
+  status: {
+    fontFamily: 'Karla-Bold',
+    fontSize: 12 * vh,
+    letterSpacing: 0.25 * vw,
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+  },
+  flagImg: {
+    width: 16 * vw,
+    height: 16 * vh,
+    marginRight: 6 * vw,
+  },
+  endText: {
+    fontFamily: 'Karla-Bold',
+    fontSize: 12 * vh,
+    lineHeight: 18 * vh,
+    letterSpacing: 0.25 * vw,
+    color: '#000000',
+    opacity: 0.6,
+  },
+  placeholder: {
+    fontFamily: 'Lato-Bold',
+    fontSize: 14 * vh,
+    lineHeight: 22 * vh,
+    letterSpacing: 0.25 * vw,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+});
+
+export default styles;
